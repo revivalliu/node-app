c71eb654974f63db288965083257efb789da8c91
Seraphina <seraphina@Seraphinas-MacBook-Air.local>
2019-06-26 12:09:57 -0700

    set mocha time out

--- test/ui/checkout.spec.js
+++ test/ui/checkout.spec.js
@@ -10,6 +10,7 @@ describe('Checkout workflow', function() {
   });
 
   it('adds a coffee to the cart and checks out', async function() {
+	this.timeout(0);
     await driver.get('http://localhost:3000');
     await driver.findElement(By.className('btn-success')).click();
     await driver.findElement(By.id('cart')).click();
