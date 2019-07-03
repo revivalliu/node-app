8c1316286ed756e2b8c7a2ffdde0cc00b1a1cacb
revivalliu <revival.liu@gmail.com>
2019-07-02 19:49:49 -0700

    change chrome driver

--- test/ui/checkout.spec.js
+++ test/ui/checkout.spec.js
@@ -4,19 +4,27 @@ var chrome = require("selenium-webdriver/chrome");
 const assert = require('assert');
 const {Builder, By} = require('selenium-webdriver');
 
-//describe('Checkout workflow', function() {
-  let driver;
+const {Builder, By, Key, until} = require('selenium-webdriver');
+ 
+(async function example() {
+  let driver = await new Builder().forBrowser('firefox').build();
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
 
-  before(async function() {
-    driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build();
-  });
+//describe('Checkout workflow', function() {
+//  let driver;
+
+//  before(async function() {
+//    driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build();
+//  });
 
 //  it('adds a coffee to the cart and checks out', async function() {
-	  driver.get('http://www.google.com/ncr');
-	  driver.findElement(By.name('q')).sendKeys('webdriver');
-	  driver.findElement(By.name('btnK')).click();
-	  driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-	  driver.quit();
 //    await driver.get('http://localhost:3000'); 
 //    await driver.findElement(By.className('btn-success')).click();
 //    await driver.findElement(By.id('cart')).click();
