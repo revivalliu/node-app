9deb28e5befff10daaeaac87109e45e357719d1d
revivalliu <revival.liu@gmail.com>
2019-07-02 19:17:45 -0700

    change chrome driver

--- test/ui/checkout.spec.js
+++ test/ui/checkout.spec.js
@@ -17,7 +17,12 @@ describe('Checkout workflow', function() {
   driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   driver.quit();
 
-//  it('adds a coffee to the cart and checks out', async function() {
+  it('adds a coffee to the cart and checks out', async function() {
+	  driver.get('http://www.google.com/ncr');
+	  driver.findElement(By.name('q')).sendKeys('webdriver');
+	  driver.findElement(By.name('btnK')).click();
+	  driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+	  driver.quit();
 //    await driver.get('http://localhost:3000'); 
 //    await driver.findElement(By.className('btn-success')).click();
 //    await driver.findElement(By.id('cart')).click();
