d66d505fd34f41d388390c0d11d9d07e0dc655ef
revivalliu <revival.liu@gmail.com>
2019-07-02 19:15:02 -0700

    change to coffee shop

--- test/ui/checkout.spec.js
+++ test/ui/checkout.spec.js
@@ -1,3 +1,4 @@
+
 require('chromedriver');
 var chrome = require("selenium-webdriver/chrome");
 const assert = require('assert');
@@ -9,14 +10,20 @@ describe('Checkout workflow', function() {
   before(async function() {
     driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build();
   });
+  
+  driver.get('http://www.google.com/ncr');
+  driver.findElement(By.name('q')).sendKeys('webdriver');
+  driver.findElement(By.name('btnK')).click();
+  driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  driver.quit();
 
-  it('adds a coffee to the cart and checks out', async function() {
-    await driver.get('http://localhost:3000');
-    await driver.findElement(By.className('btn-success')).click();
-    await driver.findElement(By.id('cart')).click();
-    await driver.findElement(By.id('checkout')).click();
+//  it('adds a coffee to the cart and checks out', async function() {
+//    await driver.get('http://localhost:3000'); 
+//    await driver.findElement(By.className('btn-success')).click();
+//    await driver.findElement(By.id('cart')).click();
+//    await driver.findElement(By.id('checkout')).click();
 
-    let total = await driver.findElement(By.id('total'));    assert.equal(await total.getText(), 'Total: $5.5');
-    });
-    after(() => driver && driver.quit());
+//    let total = await driver.findElement(By.id('total'));    assert.equal(await total.getText(), 'Total: $5.5');
+//    });
+//    after(() => driver && driver.quit());
 });
