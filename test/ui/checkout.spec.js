e1ff02cc7b615fd872133084fd1de7d6a3465705
revivalliu <revival.liu@gmail.com>
2019-07-08 06:53:59 -0700

    change chrome driver

--- test/ui/checkout.spec.js
+++ test/ui/checkout.spec.js
@@ -35,7 +35,7 @@ describe('Checkout workflow', function() {
 
   it('adds a coffee to the cart and checks out', async function() {
     await driver.get('http://localhost:3000'); 
-	await driver.findElement(By.className('btn-success')).click();
+//	await driver.findElement(By.className('btn-success')).click();
     await driver.findElement(By.id('cart')).click();
     await driver.findElement(By.id('checkout')).click();
 
